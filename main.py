13ea32a46723fcc91759d7b345516f732689ce79
Ingenium <johny.marin@udea.edu.co>
2023-02-12 09:38:50 -0500

    se agrega metodo de biseccion quemado

--- main.py
+++ main.py
@@ -1,41 +1,81 @@
 # This is a sample Python script.
 import dash
 import dash_bootstrap_components as dbc
-from dash import dcc
+from dash import dcc, dash_table
 from dash import html
 import plotly.express as px
 import numpy as np
+import pandas as pd
 from dash.dependencies import Input, Output
 from flask import Flask
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
 
 
 server = Flask(__name__)
 app = dash.Dash(server=server)
 
-# Define la función a evaluar
+def bisection(func, x_lower, x_upper, epsilon):
+    res = []
+    while abs(x_upper - x_lower) > epsilon:
+        x_middle = (x_upper + x_lower) / 2
+        y_middle = func(x_middle)
+        res.append([x_lower, x_upper, x_middle, y_middle])
+        if y_middle == 0:
+            return x_middle
+        elif y_middle * func(x_lower) < 0:
+            x_upper = x_middle
+        else:
+            x_lower = x_middle
+    res.append([x_lower, x_upper, (x_upper + x_lower) / 2, func((x_upper + x_lower) / 2)])
+    return res
+
 def func(x):
     return x**2 - 4
 
-# Define la solución encontrada por el método de bisección
-x_lower = 1
-x_upper = 2
-epsilon = 0.01
-x_middle = (x_lower + x_upper) / 2
-y_middle = func(x_middle)
-
-# Genera los datos para la gráfica
-x_vals = np.linspace(x_lower, x_upper, 100)
-y_vals = func(x_vals)
-
-# Crea la gráfica
-fig = px.line(x=x_vals, y=y_vals, title="Método de Bisección")
-fig.add_scatter(x=[x_middle], y=[y_middle], mode='markers', marker=dict(size=10, color='red'))
+results = bisection(func, 1, 2, 0.01)
 
 app.layout = html.Div([
-    dcc.Graph(figure=fig)
+    html.H1("Metodo de Bisecccion Jhonatan Restrepo - Johny Marin"),
+    html.Div([
+        html.Label("x_lower: x inferior"),
+        dcc.Input(id="input_x_lower", value=1, type="number"),
+    ]),
+    html.Div([
+       html.Label("x_upper: x superior"),
+       dcc.Input(id="input_x_upper", value=2, type="number"),
+    ]),
+    html.Div([
+       html.Label("epsilon: error"),
+       dcc.Input(id="input_epsilon", value=0.01, type="number"),
+    ]),
+    html.Div([
+       html.Label("Funcion: la funcion a optimizar"),
+       dcc.Input(id="input_func", value="x**2 - 4", type="text"),
+    ]),
+    dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in ["x_lower", "x_upper", "x_middle", "y_middle"]],
+    data=pd.DataFrame(results, columns=["x_lower", "x_upper", "x_middle", "y_middle"]).to_dict("rows"),
+    ),
 ])
+
+"""
+@app.callback(
+    dash.dependencies.Output('table', 'data'),
+    [
+        dash.dependencies.Input('input_x_lower', 'value'),
+        dash.dependencies.Input('input_x_upper', 'value'),
+        dash.dependencies.Input('input_epsilon', 'value'),
+        dash.dependencies.Input('input_func', 'value')
+    ]
+)
+def update_table(x_lower, x_upper, epsilon, func_str):
+    func = eval(f"lambda x: {func_str}")
+    res = bisection(func, x_lower, x_upper, epsilon)
+    return res
+"""
 
 if __name__ == '__main__':
     app.run_server(debug=True)
